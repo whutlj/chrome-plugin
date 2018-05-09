5a545d3ab9665031b19a77243307a2ab1b6b56f9
李杰 <lijie-02@newrank.cn>
2018-05-09 18:10:03 +0800

    update

--- js/background.js
+++ js/background.js
@@ -6,4 +6,39 @@ var pageObj ={
   admain: 'https://www.newrank.cn'
 }
 
+var expires = parseInt(new Date().getTime() / 1000 + 24 * 60 * 60) 
+// chrome.cookies.set({
+//   url: 'http://localhost:209',
+//   name: 'token',
+//   value: '123456',
+//   expirationDate: expires
+// })
 
+// setTimeout(function() {
+//   chrome.cookies.get({
+//     url: 'http://localhost:209',
+//     name: 'token'
+//   }, function (cookies) {
+//     console.log(cookies)
+//   })
+// }, 1000)
+
+function setCookie(name, value, expires) {
+  var expirationDate = null
+  if (expires) {
+    expirationDate = new Date().getTime() / 1000 + parseInt(expires) * 60 * 60
+  }
+  chrome.cookies.set({url: 'http://test.a.newrank.cn', name: name, value: value,domain:'.newrank.cn',path:'/',expirationDate: expirationDate,httpOnly: true})
+  chrome.cookies.set({url: 'http://test.a.newrank.cn', name: name, value: value,domain:'.newrank.cn',path:'/xdnphb',expirationDate: expirationDate,httpOnly: true})
+}
+
+function getCookie(name, fn) {
+  chrome.cookies.get({url: 'http://test.a.newrank.cn',name: name}, function(cookies) {
+    fn(cookies)
+  })
+}
+function getAllCookie(fn){
+  chrome.cookies.getAll({}, function(cookies){
+    fn(cookies)
+  })
+}
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,24 @@
-var background = chrome.extension.getBackgroundPage()
+var view = chrome.extension.getViews();
+var background = chrome.extension.getBackgroundPage();
 var grobalObj = {
   page: '123'
 }
 console.log(background.pageObj)
 $('.container').append('<div>'+background.pageObj.admain+'</div>').show();
-chrome.cookies.set({
-  url: 'https://www.baidu.com',
-  name: 'token',
-  value: '123456'
+
+
+
+// background.getAllCookie(function(cookies) {
+//   console.log(cookies)
+// })
+
+background.setCookie('tokens', '111122222');
+background.getCookie('tokens', function (cookie) {
+  console.log(cookie)
 })
+$('.tabGet').on('click', function () {
+  chrome.tabs.captureVisibleTab(function(dataUrl) {
+    $('.container').append('<img style="width:100%;" src="'+dataUrl+'" alt="截图" />')
+  })
+})
+
